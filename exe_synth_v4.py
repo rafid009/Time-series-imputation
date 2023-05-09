c6f4fa9e5564d2ba5cf4acb98aa4ba50377b703c
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-05-09 01:32:06 -0700

    exe added and v4 ready to run, fde-conv-multi 1-10

--- exe_synth_v4.py
+++ exe_synth_v4.py
@@ -278,9 +278,9 @@ saits = pickle.load(open(saits_model_file, 'rb'))
 
 config_dict_diffsaits = {
     'train': {
-        'epochs':4000, # 3000 -> ds3
+        'epochs':5000, # 3000 -> ds3
         'batch_size': 16 ,
-        'lr': 1.0e-2
+        'lr': 1.0e-3
     },      
     'diffusion': {
         'layers': 4, 
