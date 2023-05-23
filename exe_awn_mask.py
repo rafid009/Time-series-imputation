559986acdcc1e158624fb82500c93bf3511564bc
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-05-23 09:47:34 -0700

    learning mask for AWN done 1-6

--- exe_awn_mask.py
+++ exe_awn_mask.py
@@ -30,7 +30,7 @@ class NumpyArrayEncoder(JSONEncoder):
 seed = 10
 config_dict_csdi = {
     'train': {
-        'epochs': 2500,
+        'epochs': 10,
         'batch_size': 16 ,
         'lr': 1.0e-3
     },      
