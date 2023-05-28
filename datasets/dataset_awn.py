d5d3616e6d2d2973c3834f60f6e89bd90f133295
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-05-28 14:52:36 -0700

    awn yearly added 1-1

---
+++ datasets/dataset_awn.py
@@ -0,0 +1,153 @@
+import numpy as np
+import torch
+from torch.utils.data import DataLoader, Dataset
+
+def parse_data(sample, rate=0.3, is_test=False, length=100, include_features=None, forward_trial=-1, lte_idx=None, random_trial=False):
+    """Get mask of random points (missing at random) across channels based on k,
+    where k == number of data points. Mask of sample's shape where 0's to be imputed, and 1's to preserved
+    as per ts imputers"""
+    if isinstance(sample, torch.Tensor):
+        sample = sample.numpy()
+
+    obs_mask = ~np.isnan(sample)
+    
+    if not is_test:
+        shp = sample.shape
+        evals = sample.reshape(-1).copy()
+        indices = np.where(~np.isnan(evals))[0].tolist()
+        indices = np.random.choice(indices, int(len(indices) * rate))
+        values = evals.copy()
+        values[indices] = np.nan
+        mask = ~np.isnan(values)
+        mask = mask.reshape(shp)
+        gt_intact = values.reshape(shp).copy()
+        obs_data = np.nan_to_num(evals, copy=True)
+        obs_data = obs_data.reshape(shp)
+        # obs_data_intact = evals.reshape(shp)
+    elif random_trial:
+        evals = sample.copy()
+        values = evals.copy()
+        for i in range(evals.shape[1]):
+            indices = np.where(~np.isnan(evals[:, i]))[0].tolist()
+            indices = np.random.choice(indices, int(len(indices) * rate))
+            values[indices, i] = np.nan
+        mask = ~np.isnan(values)
+        gt_intact = values
+        obs_data = np.nan_to_num(evals, copy=True)
+    elif forward_trial != -1:
+        indices = np.where(~np.isnan(sample[:, lte_idx]))[0].tolist()
+        start = indices[forward_trial]
+        obs_data = np.nan_to_num(sample, copy=True)
+        gt_intact = sample.copy()
+        gt_intact[start:, :] = np.nan
+        mask = ~np.isnan(gt_intact)
+    else:
+        shp = sample.shape
+        evals = sample.reshape(-1).copy()
+        a = np.arange(sample.shape[0] - length)
+        # print(f"a: {a}\nsample: {sample.shape}")
+        start_idx = np.random.choice(a)
+        # print(f"random choice: {start_idx}")
+        end_idx = start_idx + length
+        obs_data_intact = sample.copy()
+        if include_features is None or len(include_features) == 0:
+            obs_data_intact[start_idx:end_idx, :] = np.nan
+        else:
+            obs_data_intact[start_idx:end_idx, include_features] = np.nan
+        mask = ~np.isnan(obs_data_intact)
+        gt_intact = obs_data_intact
+        obs_data = np.nan_to_num(evals, copy=True)
+        obs_data = obs_data.reshape(shp)
+        # obs_intact = np.nan_to_num(obs_intact, copy=True)
+    # print(f"\n\nobs data 1: {obs_data}")
+    return obs_data, obs_mask, mask, sample, gt_intact
+
+class AWN_Dataset(Dataset):
+    def __init__(self, filename, is_year=True, rate=0.1, test_index=32, is_test=False, length=100, seed=10, forward_trial=-1, random_trial=False) -> None:
+        super().__init__()
+        
+        self.observed_values = []
+        self.obs_data_intact = []
+        self.observed_masks = []
+        self.gt_masks = []
+        self.gt_intact = []
+        
+        data = np.load(filename)
+        length = 366 if is_year else 31
+        self.eval_length = length
+        indices = [i for i in range(len(data))]
+
+        test_indices = [test_index]
+        if not is_test:
+            train_indices = []
+            for i in indices:
+                if i not in test_indices:
+                    train_indices.append(i)
+            train_indices = np.array(train_indices)
+            X = data[train_indices]
+        else:
+            X = np.expand_dims(data[test_indices], axis=0)
+        self.mean = np.nanmean(X, axis=1)
+        self.std = np.nanstd(X, axis=1)
+        include_features = []
+
+        for i in range(data.shape[0]):
+            obs_val, obs_mask, mask, sample, obs_intact = parse_data(X[i], rate, is_test, length, include_features=include_features, \
+                                                                     forward_trial=forward_trial, random_trial=random_trial)
+            self.observed_values.append(obs_val)
+            self.observed_masks.append(obs_mask)
+            self.gt_masks.append(mask)
+            self.obs_data_intact.append(sample)
+            self.gt_intact.append(obs_intact)
+
+
+
+        self.gt_masks = torch.tensor(np.array(self.gt_masks), dtype=torch.float32)
+        self.observed_values = torch.tensor(np.array(self.observed_values), dtype=torch.float32)
+        self.obs_data_intact = np.array(self.obs_data_intact)
+        self.gt_intact = np.array(self.gt_intact)
+        self.observed_masks = torch.tensor(np.array(self.observed_masks), dtype=torch.float32)
+        self.observed_values = ((self.observed_values - self.mean) / self.std) * self.observed_masks
+        self.obs_data_intact = ((self.obs_data_intact - self.mean) / self.std) * self.observed_masks.numpy()
+        self.gt_intact = ((self.gt_intact - self.mean) / self.std) * self.gt_masks.numpy()
+
+        
+    def __getitem__(self, index):
+        s = {
+            "observed_data": self.observed_values[index],
+            "observed_mask": self.observed_masks[index],
+            "obs_data_intact": self.obs_data_intact[index],
+            "timepoints": np.arange(self.eval_length),
+            "gt_intact": self.gt_intact[index]
+        }
+        if len(self.gt_masks) == 0:
+            s["gt_mask"] = None
+        else:
+            s["gt_mask"] = self.gt_masks[index]
+        return s
+    
+    def __len__(self):
+        return len(self.observed_values)
+
+
+def get_dataloader(filename, batch_size=16, missing_ratio=0.1, is_test=False, test_index=32, is_year=True):
+    # np.random.seed(seed=seed)
+    train_dataset = AWN_Dataset(filename, test_index=test_index, is_year=True, rate=missing_ratio, test_index=test_index)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_dataset = AWN_Dataset(filename, test_index=test_index, rate=missing_ratio, is_year=is_year, is_test=is_test)
+    if is_test:
+        test_loader = DataLoader(test_dataset, batch_size=1)
+    else:
+        test_loader = DataLoader(test_dataset, batch_size=len(test_dataset))
+    return train_loader, test_loader
+
+
+def get_testloader_awn(filename, is_year=True, n_steps=366, batch_size=16, missing_ratio=0.2, seed=10, test_index=32, length=100, forecasting=False, random_trial=False):
+    np.random.seed(seed=seed)
+    if forecasting:
+        forward = n_steps - length
+        test_dataset = AWN_Dataset(filename, is_year=is_year, test_index=test_index, rate=missing_ratio, is_test=True, length=length, forward_trial=forward)
+    else:
+        test_dataset = AWN_Dataset(filename, is_year=is_year, test_index=test_index, rate=missing_ratio, is_test=True, length=length, random_trial=random_trial)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size)
+    return test_loader
---
+++ exe_awn.py
@@ -0,0 +1,196 @@
+from models.main_model import CSDI_Synth
+from datasets.dataset_awn import get_dataloader
+from utils.utils import train, get_num_params, evaluate_imputation_all
+import numpy as np
+import torch
+import sys
+import os
+from pypots.imputation import SAITS
+import matplotlib.pyplot as plt
+import matplotlib
+import pickle
+from datasets.dataset_awn import get_dataloader
+import json
+from json import JSONEncoder
+import math
+matplotlib.rc('xtick', labelsize=20) 
+matplotlib.rc('ytick', labelsize=20) 
+# torch.manual_seed(42)
+np.set_printoptions(threshold=sys.maxsize)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+
+given_features = feats_v4 #['sin', 'cos2', 'harmonic', 'weight', 'lin_comb', 'non_lin_comb', 'mixed_history']
+
+seed = 10
+config_dict_csdi = {
+    'train': {
+        'epochs': 2500,
+        'batch_size': 8,
+        'lr': 1.0e-3
+    },      
+    'diffusion': {
+        'layers': 4, 
+        'channels': 64,
+        'nheads': 8,
+        'diffusion_embedding_dim': 128,
+        'beta_start': 0.0001,
+        'beta_end': 0.5,
+        'num_steps': 50,
+        'schedule': "quad",
+         'is_fast': False,
+    },
+    'model': {
+        'is_unconditional': 0,
+        'timeemb': 128,
+        'featureemb': 16,
+        'target_strategy': "random",
+        'type': 'CSDI',
+        'n_layers': 3, 
+        'd_time': 100,
+        'n_feature': len(given_features),
+        'd_model': 128,
+        'd_inner': 128,
+        'n_head': 8,
+        'd_k': 64,
+        'd_v': 64,
+        'dropout': 0.1,
+        'diagonal_attention_mask': True
+    },
+}
+
+dataset_name = 'awn_daily_year'
+nsample = 50
+
+n_steps = 100
+n_features = len(given_features)
+num_seasons = 32
+noise = False
+train_loader, valid_loader = get_dataloader(n_steps, n_features, num_seasons, batch_size=16, missing_ratio=0.1, seed=10, is_test=False, v2='v4', noise=noise)
+
+model_csdi = CSDI_Synth(config_dict_csdi, device, target_dim=len(given_features)).to(device)
+model_folder = "./saved_model_synth_v4"
+filename = f"model_csdi_synth_v4{'_noise' if noise else ''}.pth"
+if not os.path.isdir(model_folder):
+    os.makedirs(model_folder)
+print(f"\n\nCSDI training starts.....\n")
+train(
+    model_csdi,
+    config_dict_csdi["train"],
+    train_loader,
+    valid_loader=valid_loader,
+    foldername=model_folder,
+    filename=f"{filename}",
+    is_saits=False
+)
+model_csdi.load_state_dict(torch.load(f"{model_folder}/{filename}"))
+print(f"CSDI params: {get_num_params(model_csdi)}")
+
+
+# saits_model_file = f"{model_folder}/saits_model_synth_v4{'_noise' if noise else ''}.pkl"
+# saits = SAITS(n_steps=n_steps, n_features=n_features, n_layers=3, d_model=256, d_inner=128, n_head=4, d_k=64, d_v=64, dropout=0.1, epochs=3000, patience=400, device=device)
+# X, mean, std = create_synthetic_data_v4(n_steps, num_seasons, seed=10, noise=noise)
+# print(f"\n\SAITS training starts.....\n")
+# saits.fit(X)
+# pickle.dump(saits, open(saits_model_file, 'wb'))
+# saits = pickle.load(open(saits_model_file, 'rb'))
+
+config_dict_diffsaits = {
+    'train': {
+        'epochs':5000, # 3000 -> ds3
+        'batch_size': 16 ,
+        'lr': 1.0e-4
+    },      
+    'diffusion': {
+        'layers': 4, 
+        'channels': 64,
+        'nheads': 8,
+        'diffusion_embedding_dim': 128,
+        'beta_start': 0.0001,
+        'beta_end': 0.5,
+        'num_steps': 50,
+        'schedule': "quad",
+         'is_fast': False,
+    },
+    'model': {
+        'is_unconditional': 0,
+        'timeemb': 128,
+        'featureemb': 16,
+        'target_strategy': "mix", # noise mix
+        'type': 'SAITS',
+        'n_layers': 8,
+        'loss_weight_p': 1,
+        'loss_weight_f': 1,
+        'd_time': n_steps,
+        'n_feature': len(given_features),
+        'd_model': 128,
+        'd_inner': 128,
+        'n_head': 8,
+        'd_k': len(given_features),
+        'd_v': len(given_features),
+        'dropout': 0.1,
+        'diagonal_attention_mask': False
+    },
+    'ablation': {
+        'fde-choice': 'fde-conv-multi',
+        'fde-layers': 4,
+        'is_fde': True,
+        'weight_combine': False,
+        'no-mask': False,
+        'fde-diagonal': True,
+        'is_fde_2nd': False,
+        'reduce-type': 'linear',
+        'is_2nd_block': True
+    }
+}
+print(f"config: {config_dict_diffsaits}")
+name = 'fde-conv-multi'
+model_diff_saits = CSDI_Synth(config_dict_diffsaits, device, target_dim=len(given_features)).to(device)
+
+filename = f"model_diffsaits_synth_v4_{name}{'_noise' if noise else ''}-mix.pth"
+print(f"\n\DiffSAITS training starts.....\n")
+
+# model_diff_saits.load_state_dict(torch.load(f"{model_folder}/{filename}"))
+
+train(
+    model_diff_saits,
+    config_dict_diffsaits["train"],
+    train_loader,
+    valid_loader=valid_loader,
+    foldername=model_folder,
+    filename=f"{filename}",
+    is_saits=True
+)
+
+# model_diff_saits.load_state_dict(torch.load(f"{model_folder}/{filename}"))
+print(f"DiffSAITS params: {get_num_params(model_diff_saits)}")
+
+models = {
+    'CSDI': model_csdi,
+    # 'SAITS': saits,
+    'DiffSAITS': model_diff_saits
+}
+mse_folder = f"results_awn_{name}{'_noise' if noise else ''}_new_2/metric"
+data_folder = f"results_synth_v4_{name}{'_noise' if noise else ''}_new_2/data"
+lengths = [10, 50, 90]
+for l in lengths:
+    print(f"\nlength = {l}")
+    print(f"\nBlackout:")
+    evaluate_imputation_all(models=models, trials=10, mse_folder=mse_folder, dataset_name='synth_v4', batch_size=32, length=l, noise=noise)
+    # evaluate_imputation_all(models=models, mse_folder=data_folder, dataset_name='synth_v4', length=l, trials=1, batch_size=1, data=True, noise=noise)
+
+print(f"\nForecasting:")
+evaluate_imputation_all(models=models, trials=10, mse_folder=mse_folder, dataset_name='synth_v4', batch_size=32, length=(10, 80), forecasting=True, noise=noise)
+# evaluate_imputation_all(models=models, mse_folder=data_folder, forecasting=True, dataset_name='synth_v4', length=l, trials=1, batch_size=1, data=True, noise=noise)
+
+miss_ratios = [0.1, 0.5, 0.9]
+for ratio in miss_ratios:
+    print(f"\nRandom Missing: ratio ({ratio})")
+    evaluate_imputation_all(models=models, trials=10, mse_folder=mse_folder, dataset_name='synth_v4', batch_size=32, missing_ratio=ratio, random_trial=True, noise=noise)
+    # evaluate_imputation_all(models=models, mse_folder=data_folder, dataset_name='synth_v4', trials=1, batch_size=1, data=True, missing_ratio=ratio, random_trial=True, noise=noise)
