836aa4ef19ced9aee6e5b9bcf224f46a22dd4450
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-05-28 19:46:17 -0700

    synthetic data experiments setup 1-5

--- exe_synth_v6.py
+++ exe_synth_v6.py
@@ -72,7 +72,7 @@ n_steps = 100
 n_features = len(given_features)
 num_seasons = 32
 noise = False
-train_loader, valid_loader = get_dataloader(n_steps, n_features, num_seasons, batch_size=16, missing_ratio=0.1, seed=10, is_test=False, v2='v2', noise=noise)
+train_loader, valid_loader = get_dataloader(n_steps, n_features, num_seasons, batch_size=16, missing_ratio=0.1, seed=10, is_test=False, v2='v6', noise=noise)
 
 model_csdi = CSDI_Synth(config_dict_csdi, device, target_dim=len(given_features)).to(device)
 model_folder = "./saved_model_synth_v6"
