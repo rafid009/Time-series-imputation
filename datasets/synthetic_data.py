corrected the ground truth dimension to (1, n_step, n_features) to properly get broadcasted for crps with forecasts 1-2

datasets/synthetic_data.py
@@ -3,8 +3,8 @@ feats = ['sin', 'cos2', 'harmonic', 'weight', 'lin_comb', 'non_lin_comb', 'mixed
 feats_v2 = ['sin', 'cos2', 'tan']
 feats_v3 = ['sin', 'cos', 'tan']
 feats_v4 = ['sin', 'cos', 'sin-cos-plus', 'sin-cos-prod']
-feats_v5 = ['sin', 'cos', 'sin-cos-hist-plus', 'sin-cos-hist-prod']
-feats_v6 = ['sin', 'sin2', 'cos', 'cos2']
+feats_v5 = ['sin', '1+sin2', 'cos', '1-cos2', 'tan', '1+tan']
+feats_v6 = ['sin', '1+sin2', 'cos', '1-cos2', 'sin2-cos2']
 
 
 def add_rn_missing(X, length=-1, rate=-1):
@@ -100,47 +100,34 @@ def create_synthetic_data_v2(n_steps, num_seasons, seed=10, rate=0.05, length_ra
     synth_features = {
         'sin': (0.00001, 2 * np.pi/3, np.pi/2), # f1
         'cos2': (0, 2*np.pi, np.pi/4), # f2
-        'tan': (0, np.pi/3, np.pi/16) # f3
+        'tan': (-np.pi/3, np.pi/3, np.pi/16) # f3
     }
     num_steps = n_steps # config['n_steps']
     num_features = len(feats_v2) # config['n_features']
     data = np.zeros((num_seasons, num_steps, num_features))
-    # value_range = [(0.1, 0.4, 0.7, 0.99), (11.0, 17.5, 40.5, 61.2), (100.1, 160.2, 500, 1000)]
-    
+
     for i in range(data.shape[0]):
-        for feature in synth_features.keys():
+        for feature in feats_v2:
             lr = int(np.floor(n_steps * (np.random.rand() * length_rate)))
             args = synth_features[feature]
             if feature == 'sin':
-                low = np.random.uniform(args[0], args[0]) + np.random.uniform(0, args[2])
-                high = np.random.uniform(args[1], args[1]) + np.random.uniform(0, args[2])
+                low = np.random.uniform(args[0], args[0] + args[2])
+                high = np.random.uniform(args[1], args[1] + args[2])
                 data[i, :, feats_v2.index(feature)] = np.sin(np.linspace(low, high, data.shape[1]))
-                
-            elif feature == 'cos':
-                low = np.random.uniform(args[0], args[0]) + np.random.uniform(0, args[2])
-                high = np.random.uniform(args[1], args[1]) + np.random.uniform(0, args[2])
-                data[i, :, feats_v2.index(feature)] = (np.cos(np.linspace(low, high, data.shape[1])))
+            elif feature == 'cos2':
+                low = np.random.uniform(args[0], args[0] + args[2])
+                high = np.random.uniform(args[1], args[1] + args[2])
+                data[i, :, feats_v2.index(feature)] = (np.cos(np.linspace(low, high, data.shape[1]) ** 2))
             elif feature == 'tan':
-                low = np.random.uniform(args[0], args[0]) + np.random.uniform(0, args[2])
-                high = np.random.uniform(args[1], args[1]) + np.random.uniform(0, args[2])
-                f1_sin = np.sin(np.linspace(low, high, data.shape[1]))
-
-                low = np.random.uniform(args[0], args[0]) + np.random.uniform(0, args[2])
-                high = np.random.uniform(args[1], args[1]) + np.random.uniform(0, args[2])
-                f2_cos = (np.cos(np.linspace(low, high, data.shape[1])) ** 2)
-
-                data[i, :, feats_v2.index(feature)] = f1_sin / (f2_cos + 1e-5) #+ np.random.rand(data.shape[1])
-            if noise:
-                noise_level = np.random.rand(data.shape[1]) * 0.5
-                data[i, :, feats_v2.index(feature)] += noise_level
-
+                low = np.random.uniform(args[0], args[0] + args[2])
+                high = np.random.uniform(args[1], args[1] + args[2])
+                data[i, :, feats_v2.index(feature)] = np.tan(np.linspace(low, high, data.shape[1]))
 
             if feature == 'sin' or feature == 'cos2' or feature == 'tan':
                 data[i, :, feats_v2.index(feature)] = add_rn_missing(data[i, :, feats_v2.index(feature)], length=lr)
             
         data[i] = add_rn_missing(data[i], rate=rate)
-            # elif feature == 'inv':
-            #     data[i, :, feats.index(feature)] = (synth_features['inv'][0] * data[i, :, feats.index('sin')] - synth_features['inv'][1] / data[i, :, feats.index('sin')])
+    
     data_rows = data.reshape((-1, num_features))
     mean = np.nanmean(data_rows, axis=0)
     std = np.nanstd(data_rows, axis=0)
@@ -153,9 +140,9 @@ def create_synthetic_data_v3(n_steps, num_seasons, seed=10, rate=0.05, length_ra
     np.random.seed(seed)
     
     synth_features = {
-        'sin': (0.0001, np.pi/3, np.pi/10), # f1
-        'cos': (0, np.pi/3, np.pi/10), # f2
-        'tan': (0, np.pi/3, np.pi/10) # f3
+        'sin': (-np.pi/3, np.pi/3, np.pi/10), # f1
+        'cos': (-np.pi/4, np.pi/3, np.pi/10), # f2
+        'tan': (0) #, np.pi/3, np.pi/10) # f3
     }
     num_steps = n_steps # config['n_steps']
     num_features = len(feats_v3) # config['n_features']
@@ -163,33 +150,31 @@ def create_synthetic_data_v3(n_steps, num_seasons, seed=10, rate=0.05, length_ra
     # value_range = [(0.1, 0.4, 0.7, 0.99), (11.0, 17.5, 40.5, 61.2), (100.1, 160.2, 500, 1000)]
     
     for i in range(data.shape[0]):
-        for feature in synth_features.keys():
+        for feature in feats_v3:
             lr = int(np.floor(n_steps * (np.random.rand() * length_rate)))
             args = synth_features[feature]
             if feature == 'sin':
-                low = np.random.uniform(args[0], args[0]) + np.random.uniform(0, args[2])
-                high = np.random.uniform(args[1], args[1]) + np.random.uniform(0, args[2])
+                low = np.random.uniform(args[0], args[0] + args[2])
+                high = np.random.uniform(args[1], args[1] + args[2])
                 data[i, :, feats_v3.index(feature)] = np.sin(np.linspace(low, high, data.shape[1]))
                 
             elif feature == 'cos':
-                low = np.random.uniform(args[0], args[0]) + np.random.uniform(0, args[2])
-                high = np.random.uniform(args[1], args[1]) + np.random.uniform(0, args[2])
+                low = np.random.uniform(args[0], args[0] + args[2])
+                high = np.random.uniform(args[1], args[1] + args[2])
                 data[i, :, feats_v3.index(feature)] = (np.cos(np.linspace(low, high, data.shape[1])))
             elif feature == 'tan':
                 f1_sin = data[i, :, feats_v3.index('sin')]
                 f2_cos = data[i, :, feats_v3.index('cos')]
-                data[i, :, feats_v3.index(feature)] = f1_sin / (f2_cos + 1e-4) #+ np.random.rand(data.shape[1])
+                data[i, :, feats_v3.index(feature)] = f1_sin / (f2_cos + 1e-5) #+ np.random.rand(data.shape[1])
             if noise and feature != 'tan':
                 noise_level = np.random.rand(data.shape[1]) * 0.5
                 data[i, :, feats_v3.index(feature)] += noise_level
 
-
-            if feature == 'sin' or feature == 'cos' or feature == 'tan':
+            if feature == 'sin' or feature == 'cos':
                 data[i, :, feats_v3.index(feature)] = add_rn_missing(data[i, :, feats_v3.index(feature)], length=lr)
             
         data[i] = add_rn_missing(data[i], rate=rate)
-            # elif feature == 'inv':
-            #     data[i, :, feats.index(feature)] = (synth_features['inv'][0] * data[i, :, feats.index('sin')] - synth_features['inv'][1] / data[i, :, feats.index('sin')])
+     
     data_rows = data.reshape((-1, num_features))
     mean = np.nanmean(data_rows, axis=0)
     std = np.nanstd(data_rows, axis=0)
@@ -213,16 +198,16 @@ def create_synthetic_data_v4(n_steps, num_seasons, seed=10, rate=0.05, length_ra
     # value_range = [(0.1, 0.4, 0.7, 0.99), (11.0, 17.5, 40.5, 61.2), (100.1, 160.2, 500, 1000)]
     
     for i in range(data.shape[0]):
-        for feature in synth_features.keys():
+        for feature in feats_v4:
             lr = int(np.floor(n_steps * (np.random.rand() * length_rate)))
             args = synth_features[feature]
             if feature == 'sin':
-                low = np.random.uniform(args[0], args[0]) + np.random.uniform(0, args[2])
-                high = np.random.uniform(args[1], args[1]) + np.random.uniform(0, args[2])
+                low = np.random.uniform(args[0], args[0] + args[2])
+                high = np.random.uniform(args[1], args[1] + args[2])
                 data[i, :, feats_v4.index(feature)] = np.sin(np.linspace(low, high, data.shape[1]))
             elif feature == 'cos':
-                low = np.random.uniform(args[0], args[0]) + np.random.uniform(0, args[2])
-                high = np.random.uniform(args[1], args[1]) + np.random.uniform(0, args[2])
+                low = np.random.uniform(args[0], args[0] + args[2])
+                high = np.random.uniform(args[1], args[1] + args[2])
                 data[i, :, feats_v4.index(feature)] = (np.cos(np.linspace(low, high, data.shape[1])))
             elif feature == 'sin-cos-plus':
                 f1_sin = data[i, :, feats_v4.index('sin')]
@@ -232,9 +217,11 @@ def create_synthetic_data_v4(n_steps, num_seasons, seed=10, rate=0.05, length_ra
                 f1 = data[i, :, feats_v4.index('sin')]
                 f2 = data[i, :, feats_v4.index('cos')]
                 data[i, :, feats_v4.index(feature)] = f1 * f2
+
             if noise and (feature != 'sin-cos-plus' or feature != 'sin-cos-prod'):
                 noise_level = np.random.rand(data.shape[1]) * 0.5
                 data[i, :, feats_v4.index(feature)] += noise_level
+
             if feature == 'sin' or feature == 'cos':
                 data[i, :, feats_v4.index(feature)] = add_rn_missing(data[i, :, feats_v4.index(feature)], length=lr)
         data[i] = add_rn_missing(data[i], rate=rate)
@@ -250,10 +237,12 @@ def create_synthetic_data_v5(n_steps, num_seasons, seed=10, rate=0.05, length_ra
     np.random.seed(seed)
     
     synth_features = {
-        'sin': (0.0001, 2*np.pi/3, np.pi/6), # f1
-        'cos': (0, np.pi, np.pi/4), # f2
-        'sin-cos-hist-plus': 0, # f3
-        'sin-cos-hist-prod': 0
+        'sin': (-np.pi/3, np.pi/3, np.pi/10), 
+        '1+sin2': (0), 
+        'cos': (-np.pi/3, np.pi/4, np.pi/10), 
+        '1-cos2': (0), 
+        'tan': (-np.pi/3, np.pi/3, np.pi/10), 
+        '1+tan': (0)
     }
     num_steps = n_steps # config['n_steps']
     num_features = len(feats_v5) # config['n_features']
@@ -261,37 +250,35 @@ def create_synthetic_data_v5(n_steps, num_seasons, seed=10, rate=0.05, length_ra
     # value_range = [(0.1, 0.4, 0.7, 0.99), (11.0, 17.5, 40.5, 61.2), (100.1, 160.2, 500, 1000)]
     
     for i in range(data.shape[0]):
-        for feature in synth_features.keys():
+        for feature in feats_v5:
             lr = int(np.floor(n_steps * (np.random.rand() * length_rate)))
             args = synth_features[feature]
             if feature == 'sin':
-                low = np.random.uniform(args[0], args[0]) + np.random.uniform(0, args[2])
-                high = np.random.uniform(args[1], args[1]) + np.random.uniform(0, args[2])
+                low = np.random.uniform(args[0], args[0] + args[2])
+                high = np.random.uniform(args[1], args[1] + args[2])
                 data[i, :, feats_v5.index(feature)] = np.sin(np.linspace(low, high, data.shape[1]))
+            elif feature == '1+sin2':
+                f1 = data[i, :, feats_v5.index('sin')]
+                data[i, :, feats_v5.index(feature)] = 1 + f1 ** 2
             elif feature == 'cos':
-                low = np.random.uniform(args[0], args[0]) + np.random.uniform(0, args[2])
-                high = np.random.uniform(args[1], args[1]) + np.random.uniform(0, args[2])
+                low = np.random.uniform(args[0], args[0] + args[2])
+                high = np.random.uniform(args[1], args[1] + args[2])
                 data[i, :, feats_v5.index(feature)] = (np.cos(np.linspace(low, high, data.shape[1])))
-            elif feature == 'sin-cos-hist-plus':
-                data[i, 0, feats_v5.index(feature)] = data[i, 0, feats_v5.index('sin')]
-                f1_sin = data[i, 1:, feats_v5.index('sin')]
-                f2_cos = data[i, 1:, feats_v5.index('cos')]
-                data[i, 1:, feats_v5.index(feature)] = f1_sin + f2_cos
-            elif feature == 'sin-cos-hist-prod':
-                data[i, 0, feats_v5.index(feature)] = data[i, 0, feats_v5.index('cos')]
-                f1_sin = data[i, 1:, feats_v5.index('sin')]
-                f2_cos = data[i, 1:, feats_v5.index('cos')]
-                data[i, 1:, feats_v5.index(feature)] = f1_sin * f2_cos
-            if noise and (feature != 'sin-cos-hist-plus' or feature != 'sin-cos-hist-prod'):
-                noise_level = np.random.rand(data.shape[1]) * 0.5
-                data[i, :, feats_v5.index(feature)] += noise_level
-                
-            if feature == 'sin' or feature == 'cos':
+            elif feature == '1-cos2':
+                f3 = data[i, :, feats_v5.index('cos')]
+                data[i, 1:, feats_v5.index(feature)] = 1 - f3 ** 2
+            elif feature == 'tan':
+                low = np.random.uniform(args[0], args[0] + args[2])
+                high = np.random.uniform(args[1], args[1] + args[2])
+                data[i, :, feats_v5.index(feature)] = (np.tan(np.linspace(low, high, data.shape[1])))
+            elif feature == '1+tan':
+                f5 = data[i, :, feats_v5.index('tan')]
+                data[i, :, feats_v5.index(feature)] = 1 + f5
+            if feature == 'sin' or feature == 'cos' or feature == 'tan':
                 data[i, :, feats_v5.index(feature)] = add_rn_missing(data[i, :, feats_v5.index(feature)], length=lr)
             
         data[i] = add_rn_missing(data[i], rate=rate)
-            # elif feature == 'inv':
-            #     data[i, :, feats.index(feature)] = (synth_features['inv'][0] * data[i, :, feats.index('sin')] - synth_features['inv'][1] / data[i, :, feats.index('sin')])
+    
     data_rows = data.reshape((-1, num_features))
     mean = np.nanmean(data_rows, axis=0)
     std = np.nanstd(data_rows, axis=0)
@@ -303,10 +290,11 @@ def create_synthetic_data_v6(n_steps, num_seasons, seed=10, rate=0.05, length_ra
     np.random.seed(seed)
     
     synth_features = {
-        'sin': (0.0001, 2*np.pi/3, np.pi/6), # f1
-        'cos': (0, np.pi, np.pi/4), # f2
-        'sin2': 0, # f3
-        'cos2': 0
+        'sin': (-np.pi/3, np.pi/3, np.pi/16), 
+        '1+sin2': (0), 
+        'cos': (-np.pi/3, np.pi/3, np.pi/16), 
+        '1-cos2': (0), 
+        'cos2-sin2': (0)
     }
     num_steps = n_steps # config['n_steps']
     num_features = len(feats_v5) # config['n_features']
@@ -314,33 +302,33 @@ def create_synthetic_data_v6(n_steps, num_seasons, seed=10, rate=0.05, length_ra
     # value_range = [(0.1, 0.4, 0.7, 0.99), (11.0, 17.5, 40.5, 61.2), (100.1, 160.2, 500, 1000)]
     
     for i in range(data.shape[0]):
-        for feature in synth_features.keys():
+        for feature in feats_v6:
             lr = int(np.floor(n_steps * (np.random.rand() * length_rate)))
             args = synth_features[feature]
             if feature == 'sin':
-                low = np.random.uniform(args[0], args[0]) + np.random.uniform(0, args[2])
-                high = np.random.uniform(args[1], args[1]) + np.random.uniform(0, args[2])
-                data[i, :, feats_v5.index(feature)] = np.sin(np.linspace(low, high, data.shape[1]))
+                low = np.random.uniform(args[0], args[0] + args[2])
+                high = np.random.uniform(args[1], args[1] + args[2])
+                data[i, :, feats_v6.index(feature)] = np.sin(np.linspace(low, high, data.shape[1]))
+            elif feature == '1+sin2':
+                f1 = data[i, :, feats_v6.index('sin')]
+                data[i, :, feats_v6.index(feature)] = 1 + f1 ** 2
             elif feature == 'cos':
-                low = np.random.uniform(args[0], args[0]) + np.random.uniform(0, args[2])
-                high = np.random.uniform(args[1], args[1]) + np.random.uniform(0, args[2])
-                data[i, :, feats_v5.index(feature)] = (np.cos(np.linspace(low, high, data.shape[1])))
-            elif feature == 'sin2':
-                f1_sin = data[i, :, feats_v5.index('sin')]
-                data[i, :, feats_v5.index(feature)] = 1 - f1_sin 
-            elif feature == 'cos2':
-                f2_cos = data[i, :, feats_v5.index('cos')]
-                data[i, :, feats_v5.index(feature)] = 1 - 5 * f2_cos
-            if noise and (feature != 'sin2' or feature != 'cos2'):
-                noise_level = np.random.rand(data.shape[1]) * 0.5
-                data[i, :, feats_v5.index(feature)] += noise_level
-                
+                low = np.random.uniform(args[0], args[0] + args[2])
+                high = np.random.uniform(args[1], args[1] + args[2])
+                data[i, :, feats_v6.index(feature)] = (np.cos(np.linspace(low, high, data.shape[1])))
+            elif feature == '1-cos2':
+                f3 = data[i, :, feats_v6.index('cos')]
+                data[i, 1:, feats_v6.index(feature)] = 1 - f3 ** 2
+            elif feature == 'cos2-sin2':
+                f2 = data[i, :, feats_v6.index('1+sin2')]
+                f4 = data[i, :, feats_v6.index('1-cos2')]
+                data[i, :, feats_v5.index(feature)] = f2 / f4
+            
             if feature == 'sin' or feature == 'cos':
-                data[i, :, feats_v5.index(feature)] = add_rn_missing(data[i, :, feats_v5.index(feature)], length=lr)
+                data[i, :, feats_v6.index(feature)] = add_rn_missing(data[i, :, feats_v6.index(feature)], length=lr)
             
         data[i] = add_rn_missing(data[i], rate=rate)
-            # elif feature == 'inv':
-            #     data[i, :, feats.index(feature)] = (synth_features['inv'][0] * data[i, :, feats.index('sin')] - synth_features['inv'][1] / data[i, :, feats.index('sin')])
+    
     data_rows = data.reshape((-1, num_features))
     mean = np.nanmean(data_rows, axis=0)
     std = np.nanstd(data_rows, axis=0)

exe_synth_v4.py
@@ -26,181 +26,7 @@ class NumpyArrayEncoder(JSONEncoder):
             return obj.tolist()
         return JSONEncoder.default(self, obj)
 
-given_features = feats_v4 #['sin', 'cos2', 'harmonic', 'weight', 'lin_comb', 'non_lin_comb', 'mixed_history']
-
-def evaluate_imputation(models, mse_folder, exclude_key='', exclude_features=None, trials=20, length=100, season_idx=None, random_trial=False, forward_trial=False, data=False, missing_ratio=0.2):
-    # given_features = given_features = ['sin', 'cos2', 'harmonic', 'weight', 'inv'] 
-    nsample = 30
-    # trials = 30
-    season_avg_mse = {}
-    num_seasons = 1
-    n_steps = 100
-    # exclude_features = ['MEAN_AT', 'MIN_AT', 'AVG_AT', 'MAX_AT']
-    results = {}
-    models['CSDI'].eval()
-    models['DiffSAITS'].eval()
-
-    for season in range(num_seasons):
-        print(f"For season: {season}")
-        mse_csdi_total = {}
-        mse_saits_total = {}
-        mse_diff_saits_total = {}
-        CRPS_csdi = 0
-        CRPS_diff_saits = 0
-        for i in range(trials):
-            test_loader = get_testloader(n_steps, len(given_features), 1, exclude_features=exclude_features, length=length, seed=5*i, forward_trial=forward_trial, random_trial=random_trial, missing_ratio=missing_ratio)
-            
-            for j, test_batch in enumerate(test_loader, start=1):
-                if 'CSDI' in models.keys():
-                    output = models['CSDI'].evaluate(test_batch, nsample)
-                    samples, c_target, eval_points, observed_points, observed_time, obs_intact, gt_intact = output
-                    samples = samples.permute(0, 1, 3, 2)  # (B,nsample,L,K)
-                    c_target = c_target.permute(0, 2, 1)  # (B,L,K)
-                    eval_points = eval_points.permute(0, 2, 1)
-                    observed_points = observed_points.permute(0, 2, 1)
-                    samples_median = samples.median(dim=1)
-                
-                if 'DiffSAITS' in models.keys():
-                    output_diff_saits = models['DiffSAITS'].evaluate(test_batch, nsample)
-                    if 'CSDI' not in models.keys():
-                        samples_diff_saits, c_target, eval_points, observed_points, observed_time, obs_intact, gt_intact = output_diff_saits
-                        c_target = c_target.permute(0, 2, 1)  # (B,L,K)
-                        eval_points = eval_points.permute(0, 2, 1)
-                        observed_points = observed_points.permute(0, 2, 1)
-                    else:
-                        samples_diff_saits, _, _, _, _, _, _ = output_diff_saits
-                    samples_diff_saits = samples_diff_saits.permute(0, 1, 3, 2)
-                    samples_diff_saits_median = samples_diff_saits.median(dim=1)
-                    samples_diff_saits_mean = samples_diff_saits.mean(dim=1)
-
-                # gt_intact = gt_intact.squeeze(axis=0)
-                saits_X = gt_intact #test_batch['obs_data_intact']
-                saits_output = models['SAITS'].impute(saits_X)
-                
-                if data:
-                    if 'CSDI' in models.keys():
-                        results[season] = {
-                            'target mask': eval_points[0, :, :].cpu().numpy(),
-                            'target': c_target[0, :, :].cpu().numpy(),
-                            # 'csdi_mean': samples_mean[0, :, :].cpu().numpy(),
-                            'csdi_median': samples_median.values[0, :, :].cpu().numpy(),
-                            'csdi_samples': samples[0].cpu().numpy(),
-                            'saits': saits_output[0, :, :],
-                            'diff_saits_mean': samples_diff_saits_mean[0, :, :].cpu().numpy(),
-                            'diff_saits_median': samples_diff_saits_median.values[0, :, :].cpu().numpy(),
-                            'diff_saits_samples': samples_diff_saits[0].cpu().numpy(),
-                            # 'diff_saits_median_simple': samples_diff_saits_median_simple.values[0, :, :].cpu().numpy(),
-                            # 'diff_saits_samples_simple': samples_diff_saits_simple[0].cpu().numpy()
-                            }
-                    else:
-                         results[season] = {
-                            'target mask': eval_points[0, :, :].cpu().numpy(),
-                            'target': c_target[0, :, :].cpu().numpy(),
-                            'saits': saits_output[0, :, :],
-                            'diff_saits_mean': samples_diff_saits_mean[0, :, :].cpu().numpy(),
-                            # 'diff_saits_median': samples_diff_saits_median.values[0, :, :].cpu().numpy(),
-                            'diff_saits_samples': samples_diff_saits[0].cpu().numpy(),
-                        }
-                else:
-                    for feature in given_features:
-                        if exclude_features is not None and feature in exclude_features:
-                            continue
-                        # print(f"For feature: {feature}, for length: {length}, trial: {random_trial}")
-                        feature_idx = given_features.index(feature)
-                        if eval_points[0, :, feature_idx].sum().item() == 0:
-                            continue
-                        if 'CSDI' in models.keys():
-                            mse_csdi = ((samples_median.values[0, :, feature_idx] - c_target[0, :, feature_idx]) * eval_points[0, :, feature_idx]) ** 2
-                            mse_csdi = math.sqrt(mse_csdi.sum().item() / eval_points[0, :, feature_idx].sum().item())
-
-                            mae_csdi = torch.abs((samples_median.values[0, :, feature_idx] - c_target[0, :, feature_idx]) * eval_points[0, :, feature_idx])
-                            mae_csdi = mae_csdi.sum().item() / eval_points[0, :, feature_idx].sum().item()
-                            
-                            if feature not in mse_csdi_total.keys():
-                                mse_csdi_total[feature] = {'rmse': 0, 'mae': 0}
-                            
-                            mse_csdi_total[feature]["rmse"] += mse_csdi
-                            mse_csdi_total[feature]['mae'] += mae_csdi
-
-                        if feature not in mse_diff_saits_total.keys():
-                            mse_diff_saits_total[feature] = {'rmse': 0, 'mae': 0, 'diff_rmse_med': 0}
-
-                        mse_diff_saits = ((samples_diff_saits_mean[0, :, feature_idx] - c_target[0, :, feature_idx]) * eval_points[0, :, feature_idx]) ** 2
-                        mse_diff_saits = math.sqrt(mse_diff_saits.sum().item() / eval_points[0, :, feature_idx].sum().item())
-
-                        mse_diff_saits_median = ((samples_diff_saits_median.values[0, :, feature_idx] - c_target[0, :, feature_idx]) * eval_points[0, :, feature_idx]) ** 2
-                        mse_diff_saits_median = math.sqrt(mse_diff_saits_median.sum().item() / eval_points[0, :, feature_idx].sum().item())
-
-                        mae_diff_saits = torch.abs((samples_diff_saits_mean[0, :, feature_idx] - c_target[0, :, feature_idx]) * eval_points[0, :, feature_idx])
-                        mae_diff_saits = mae_diff_saits.sum().item() / eval_points[0, :, feature_idx].sum().item()
-                        
-                        mse_diff_saits_total[feature]["rmse"] += mse_diff_saits
-                        mse_diff_saits_total[feature]["mae"] += mae_diff_saits
-                        mse_diff_saits_total[feature]["diff_rmse_med"] += mse_diff_saits_median
-
-
-                        mse_saits = ((torch.tensor(saits_output[0, :, feature_idx], device=device)- c_target[0, :, feature_idx]) * eval_points[0, :, feature_idx]) ** 2
-                        mae_saits = torch.abs((torch.tensor(saits_output[0, :, feature_idx], device=device)- c_target[0, :, feature_idx]) * eval_points[0, :, feature_idx])
-                        mse_saits = math.sqrt(mse_saits.sum().item() / eval_points[0, :, feature_idx].sum().item())
-                        mae_saits = mae_saits.sum().item() / eval_points[0, :, feature_idx].sum().item()
-
-                        if feature not in mse_saits_total.keys():
-                            mse_saits_total[feature] = {'rmse': 0, 'mae': 0}
-
-                        mse_saits_total[feature]['rmse'] += mse_saits
-                        mse_saits_total[feature]['mae'] += mae_saits
-                CRPS_csdi += calc_quantile_CRPS(c_target, samples, eval_points, 0, 1)
-                CRPS_diff_saits += calc_quantile_CRPS(c_target, samples_diff_saits, eval_points, 0, 1)
-        print(f"CSDI CRPS: {CRPS_csdi/trials}")
-        print(f"DiffSAITS CRPS: {CRPS_diff_saits/trials}")
-        if not data:
-            print(f"For season = {season}:")
-            for feature in given_features:
-                if exclude_features is not None and feature in exclude_features:
-                    continue
-                # if feature not in mse_csdi_total.keys() or feature not in mse_diff_saits_total.keys():
-                #     continue
-                if 'CSDI' in models.keys():
-                    for i in mse_csdi_total[feature].keys():
-                        mse_csdi_total[feature][i] /= trials
-                for i in mse_diff_saits_total[feature].keys():
-                    mse_diff_saits_total[feature][i] /= trials
-                for i in mse_saits_total[feature].keys():
-                    mse_saits_total[feature][i] /= trials
-                if 'CSDI' in models.keys():
-                    print(f"\n\tFor feature = {feature}\n\tCSDI mae: {mse_csdi_total[feature]['mae']}\n\tDiffSAITS mae: {mse_diff_saits_total[feature]['mae']}")
-                    print(f"\n\tFor feature = {feature}\n\tCSDI rmse: {mse_csdi_total[feature]['rmse']}\n\tDiffSAITS rmse: {mse_diff_saits_total[feature]['rmse']}\n\tDiffSAITS median: {mse_diff_saits_total[feature]['diff_rmse_med']}\n\tSAITS mse: {mse_saits_total[feature]['rmse']}")# \
-                else:
-                    print(f"\n\tFor feature = {feature}\n\tDiffSAITS mae: {mse_diff_saits_total[feature]['mae']}")
-                    print(f"\n\tFor feature = {feature}\n\tDiffSAITS mse: {mse_diff_saits_total[feature]['rmse']}")# \
-                
-                # DiffSAITSsimple mse: {mse_diff_saits_simple_total[feature]}")
-                # except:
-                #     continue
-            if 'CSDI' in models.keys():
-                season_avg_mse[season] = {
-                    'CSDI': mse_csdi_total,
-                    'SAITS': mse_saits_total,
-                    'DiffSAITS': mse_diff_saits_total#,
-                    # 'DiffSAITSsimple': mse_diff_saits_simple_total
-                }
-            else:
-                season_avg_mse[season] = {
-                    'SAITS': mse_saits_total,
-                    'DiffSAITS': mse_diff_saits_total#,
-                }
-
-    if not os.path.isdir(mse_folder):
-        os.makedirs(mse_folder)
-    if data:
-        fp = open(f"{mse_folder}/samples-{exclude_key if len(exclude_key) != 0 else 'all'}-{length}_random_{random_trial}_forecast_{forward_trial}_miss_{missing_ratio}.json", "w")
-        json.dump(results, fp=fp, indent=4, cls=NumpyArrayEncoder)
-        fp.close()
-    else:
-        out_file = open(f"{mse_folder}/mse_mae_{exclude_key if len(exclude_key) != 0 else 'all'}_{length}_random_{random_trial}_forecast_{forward_trial}_miss_{missing_ratio}.json", "w")
-        json.dump(season_avg_mse, out_file, indent = 4)
-        out_file.close()
-   
+given_features = feats_v4 #['sin', 'cos2', 'harmonic', 'weight', 'lin_comb', 'non_lin_comb', 'mixed_history']   
 
 seed = 10
 config_dict_csdi = {
@@ -253,28 +79,28 @@ filename = f"model_csdi_synth_v4{'_noise' if noise else ''}.pth"
 if not os.path.isdir(model_folder):
     os.makedirs(model_folder)
 print(f"\n\nCSDI training starts.....\n")
-# train(
-#     model_csdi,
-#     config_dict_csdi["train"],
-#     train_loader,
-#     valid_loader=valid_loader,
-#     foldername=model_folder,
-#     filename=f"{filename}",
-#     is_saits=False
-# )
+train(
+    model_csdi,
+    config_dict_csdi["train"],
+    train_loader,
+    valid_loader=valid_loader,
+    foldername=model_folder,
+    filename=f"{filename}",
+    is_saits=False
+)
 model_csdi.load_state_dict(torch.load(f"{model_folder}/{filename}"))
 print(f"CSDI params: {get_num_params(model_csdi)}")
 
 
-saits_model_file = f"{model_folder}/saits_model_synth_v4{'_noise' if noise else ''}.pkl"
-saits = SAITS(n_steps=n_steps, n_features=n_features, n_layers=3, d_model=256, d_inner=128, n_head=4, d_k=64, d_v=64, dropout=0.1, epochs=3000, patience=400, device=device)
+# saits_model_file = f"{model_folder}/saits_model_synth_v4{'_noise' if noise else ''}.pkl"
+# saits = SAITS(n_steps=n_steps, n_features=n_features, n_layers=3, d_model=256, d_inner=128, n_head=4, d_k=64, d_v=64, dropout=0.1, epochs=3000, patience=400, device=device)
 # X, mean, std = create_synthetic_data_v4(n_steps, num_seasons, seed=10, noise=noise)
 # print(f"\n\SAITS training starts.....\n")
 # saits.fit(X)
 # pickle.dump(saits, open(saits_model_file, 'wb'))
 
 
-saits = pickle.load(open(saits_model_file, 'rb'))
+# saits = pickle.load(open(saits_model_file, 'rb'))
 
 config_dict_diffsaits = {
     'train': {
@@ -328,7 +154,7 @@ print(f"config: {config_dict_diffsaits}")
 name = 'fde-conv-multi'
 model_diff_saits = CSDI_Synth(config_dict_diffsaits, device, target_dim=len(given_features)).to(device)
 
-filename = f"model_diffsaits_synth_v4_{name}{'_noise' if noise else ''}-mix.pth"
+filename = f"model_diffsaits_synth_v4_{name}{'_noise' if noise else ''}_new.pth"
 print(f"\n\DiffSAITS training starts.....\n")
 
 # model_diff_saits.load_state_dict(torch.load(f"{model_folder}/{filename}"))
@@ -348,11 +174,11 @@ print(f"DiffSAITS params: {get_num_params(model_diff_saits)}")
 
 models = {
     'CSDI': model_csdi,
-    'SAITS': saits,
+    # 'SAITS': saits,
     'DiffSAITS': model_diff_saits
 }
-mse_folder = f"results_synth_v4_{name}{'_noise' if noise else ''}_new_2/metric"
-data_folder = f"results_synth_v4_{name}{'_noise' if noise else ''}_new_2/data"
+mse_folder = f"results_synth_v4_{name}{'_noise' if noise else ''}_new/metric"
+data_folder = f"results_synth_v4_{name}{'_noise' if noise else ''}_new/data"
 lengths = [10, 50, 90]
 for l in lengths:
     print(f"\nlength = {l}")

models/main_model.py
@@ -70,8 +70,8 @@ class CSDI_base(nn.Module):
         if self.is_saits:
             self.loss_weight_p = config['model']['loss_weight_p']
             self.loss_weight_f = config['model']['loss_weight_f']
-        self.is_miss_pattern = config['model']['is_pattern']
-        if self.is_miss_pattern:
+
+        if self.target_strategy == 'pattern':
             self.pattern_i = 0
             self.pattern_folder = config['model']['pattern_dir']
             self.num_patterns = config['model']['num_patterns']
@@ -117,14 +117,8 @@ class CSDI_base(nn.Module):
         self.pattern_i = (self.pattern_i + 1) % self.num_patterns
         cond_mask = ((pattern - observed_mask) > 0).float()
         if np.count_nonzero(cond_mask) == 0:
-            if self.target_strategy != "random":
-                cond_mask = self.get_hist_mask(
-                    observed_mask, for_pattern_mask=observed_mask
-                )
-            else:
-                cond_mask = self.get_randmask(observed_mask)
+            cond_mask = self.get_randmask(observed_mask)
         return cond_mask
-
 
 
     def get_side_info(self, observed_tp, cond_mask):
@@ -288,7 +282,9 @@ class CSDI_base(nn.Module):
         ) = self.process_data(batch)
         if is_train == 0:
             cond_mask = gt_mask
-        elif self.target_strategy != "random":
+        elif self.target_strategy == 'pattern':
+            cond_mask = self.get_pattern_mask(observed_data)
+        elif self.target_strategy == "mix":
             cond_mask = self.get_hist_mask(
                 observed_mask, for_pattern_mask=for_pattern_mask
             )