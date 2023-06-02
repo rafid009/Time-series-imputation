7386900cafc298a0199ade9d7137fee8cdfc36f5
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-06-02 09:32:07 -0700

    physio with pattern 1-5

--- exe_physio_mask.py
+++ exe_physio_mask.py
@@ -74,17 +74,18 @@ model_folder = "saved_model_physio_mask"
 filename = "model_csdi_physio_mask.pth"
 if not os.path.isdir(model_folder):
     os.makedirs(model_folder)
-train(
-    model_csdi,
-    config["train"],
-    train_loader,
-    valid_loader=valid_loader,
-    foldername=model_folder,
-    filename=filename
-)
+# train(
+#     model_csdi,
+#     config["train"],
+#     train_loader,
+#     valid_loader=valid_loader,
+#     foldername=model_folder,
+#     filename=filename
+# )
 
+model_csdi.load_state_dict(torch.load(f"{model_folder}/{filename}"))
 
-nsample = 30000 # 3000 * 4 * 8
+nsample = 40000 # 3000 * 4 * 8
 ground = 0
 for i, val in enumerate(test_loader):
     ground = val['observed_mask'].to(args["device"]).float() # (B, L, K)
