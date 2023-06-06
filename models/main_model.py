7ca6fe0da0ec9e652a95742070fe55cbb5e9795a
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-06-06 09:52:42 -0700

    AWN pattern plus model lr=5e-4 1-6

--- models/main_model.py
+++ models/main_model.py
@@ -119,14 +119,14 @@ class CSDI_base(nn.Module):
         patterns = []
         for i in range(B):
             if is_val:
-                # pattern = np.load(f"{self.pattern_folder}/pattern_{self.val_pattern_i}.npy")
-                pattern = np.load(f"{self.pattern_folder}/pattern_1.npy")
+                pattern = np.load(f"{self.pattern_folder}/pattern_{self.val_pattern_i}.npy")
+                # pattern = np.load(f"{self.pattern_folder}/pattern_1.npy")
                 self.val_pattern_i = self.val_pattern_i + 1
                 if self.val_pattern_i >= self.num_patterns:
                     self.val_pattern_i = self.num_patterns
             else:
-                # pattern = np.load(f"{self.pattern_folder}/pattern_{self.pattern_i}.npy")
-                pattern = np.load(f"{self.pattern_folder}/pattern_1.npy")
+                pattern = np.load(f"{self.pattern_folder}/pattern_{self.pattern_i}.npy")
+                # pattern = np.load(f"{self.pattern_folder}/pattern_1.npy")
                 self.pattern_i = (self.pattern_i + 1) % self.num_patterns
             pattern = torch.tensor(pattern, dtype=torch.float32)
             patterns.append(pattern)
