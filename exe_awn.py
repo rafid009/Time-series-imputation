e5bfd2df2399a7705ceace4d9a2933c981394b86
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-06-05 22:17:49 -0700

    AWN pattern plus model 1-1

--- exe_awn.py
+++ exe_awn.py
@@ -102,7 +102,8 @@ config_dict_csdi_random = {
         'd_v': 64,
         'dropout': 0.1,
         'diagonal_attention_mask': True,
-        'num_patterns': 10000,
+        'num_patterns': 15000,
+        'num_val_pattern': 5000,
         'pattern_dir': './data/Daily/miss_pattern'
     },
 }
