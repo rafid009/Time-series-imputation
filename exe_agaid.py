aba2f0244353786acebd493023c2dedf3a377a79
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-06-17 08:11:11 -0700

    pattern mask corrected in testing 1-2

--- exe_agaid.py
+++ exe_agaid.py
@@ -113,16 +113,16 @@ model_folder = "./saved_model_agaid"
 if not os.path.isdir(model_folder):
     os.makedirs(model_folder)
 filename = f'model_csdi_{miss_type}.pth'
-train(
-    model_csdi,
-    config_dict_csdi["train"],
-    train_loader,
-    valid_loader=valid_loader,
-    foldername=model_folder,
-    filename=filename
-)
+# train(
+#     model_csdi,
+#     config_dict_csdi["train"],
+#     train_loader,
+#     valid_loader=valid_loader,
+#     foldername=model_folder,
+#     filename=filename
+# )
 # nsample = 50
-# model_csdi.load_state_dict(torch.load(f"{model_folder}/{filename}"))
+model_csdi.load_state_dict(torch.load(f"{model_folder}/{filename}"))
 print(f"CSDI params: {get_num_params(model_csdi)}")
 # evaluate(model_csdi, valid_loader, nsample=nsample, scaler=1, foldername=model_folder)
 # model_folder_exp = "./saved_model_explode"
