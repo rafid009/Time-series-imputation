6bf09f872743c5542ec596d8893df9a940211b9a
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-06-21 21:06:58 -0700

    synth v3 mask model 1-2

---
+++ datasets/dataset_synth_mask.py
@@ -0,0 +1,175 @@
+import numpy as np
+import torch
+from torch.utils.data import DataLoader, Dataset
+import os
+from datasets.synthetic_data import create_synthetic_data, create_synthetic_data_v2, create_synthetic_data_v3, create_synthetic_data_v4, create_synthetic_data_v5, create_synthetic_data_v6, create_synthetic_data_v7
+
+def parse_data(sample, rate=0.3, is_test=False, length=100, include_features=None, forward_trial=-1, lte_idx=None, random_trial=False):
+    """Get mask of random points (missing at random) across channels based on k,
+    where k == number of data points. Mask of sample's shape where 0's to be imputed, and 1's to preserved
+    as per ts imputers"""
+    if isinstance(sample, torch.Tensor):
+        sample = sample.numpy()
+
+    obs_mask = ~np.isnan(sample)
+    
+    if not is_test:
+        shp = sample.shape
+        evals = sample.reshape(-1).copy()
+        indices = np.where(~np.isnan(evals))[0].tolist()
+        indices = np.random.choice(indices, int(len(indices) * rate))
+        values = evals.copy()
+        values[indices] = np.nan
+        mask = ~np.isnan(values)
+        mask = mask.reshape(shp)
+        gt_intact = values.reshape(shp).copy()
+        obs_data = np.nan_to_num(evals, copy=True)
+        obs_data = obs_data.reshape(shp)
+        # obs_data_intact = evals.reshape(shp)
+    elif random_trial:
+        evals = sample.copy()
+        values = evals.copy()
+        for i in range(evals.shape[1]):
+            indices = np.where(~np.isnan(evals[:, i]))[0].tolist()
+            indices = np.random.choice(indices, int(len(indices) * rate))
+            values[indices, i] = np.nan
+        mask = ~np.isnan(values)
+        gt_intact = values
+        obs_data = np.nan_to_num(evals, copy=True)
+    elif forward_trial != -1:
+        indices = np.where(~np.isnan(sample[:, lte_idx]))[0].tolist()
+        start = indices[forward_trial]
+        obs_data = np.nan_to_num(sample, copy=True)
+        gt_intact = sample.copy()
+        gt_intact[start:, :] = np.nan
+        mask = ~np.isnan(gt_intact)
+    else:
+        shp = sample.shape
+        evals = sample.reshape(-1).copy()
+        a = np.arange(sample.shape[0] - length)
+        # print(f"a: {a}\nsample: {sample.shape}")
+        start_idx = np.random.choice(a)
+        # print(f"random choice: {start_idx}")
+        end_idx = start_idx + length
+        obs_data_intact = sample.copy()
+        if include_features is None or len(include_features) == 0:
+            obs_data_intact[start_idx:end_idx, :] = np.nan
+        else:
+            obs_data_intact[start_idx:end_idx, include_features] = np.nan
+        mask = ~np.isnan(obs_data_intact)
+        gt_intact = obs_data_intact
+        obs_data = np.nan_to_num(evals, copy=True)
+        obs_data = obs_data.reshape(shp)
+        # obs_intact = np.nan_to_num(obs_intact, copy=True)
+    # print(f"\n\nobs data 1: {obs_data}")
+    return obs_data, obs_mask, mask, sample, gt_intact
+
+class Synth_Dataset(Dataset):
+    def __init__(self, n_steps, n_features, num_seasons, rate=0.1, is_test=False, length=100, exclude_features=None, seed=10, forward_trial=-1, random_trial=False, v2='v1', noise=False, mean=None, std=None, is_mcar=False, is_col_miss=None) -> None:
+        super().__init__()
+        self.eval_length = n_steps
+        self.observed_values = []
+        self.obs_data_intact = []
+        self.observed_masks = []
+        self.gt_masks = []
+        self.gt_intact = []
+        if v2 == 'v1':
+            X, mu, sigma = create_synthetic_data(n_steps, num_seasons, seed=seed)
+        elif v2 == 'v2':
+            X, mu, sigma = create_synthetic_data_v2(n_steps, num_seasons, seed=seed, noise=noise)
+        elif v2 == 'v4':
+            X, mu, sigma = create_synthetic_data_v4(n_steps, num_seasons, seed=seed, noise=noise)
+        elif v2 == 'v5':
+            X, mu, sigma = create_synthetic_data_v5(n_steps, num_seasons, seed=seed, noise=noise)
+        elif v2 == 'v6':
+            X, mu, sigma = create_synthetic_data_v6(n_steps, num_seasons, seed=seed, noise=noise)
+        elif v2 == 'v7':
+            X, mu, sigma = create_synthetic_data_v7(n_steps, num_seasons, seed=seed, noise=noise)
+        elif v2 == 'v3':
+            X, mu, sigma = create_synthetic_data_v3(n_steps, num_seasons, seed=seed, noise=noise, is_mcar=is_mcar, is_col_miss=is_col_miss)
+        
+        if mean is not None and std is not None:
+            self.mean = mean
+            self.std = std
+        else:
+            self.mean = mu
+            self.std = sigma
+            synth_dir = f"./data/synth/{v2}"
+            if not os.path.isdir(synth_dir):
+                os.makedirs(synth_dir)
+            np.save(f"{synth_dir}/mean.npy", self.mean)
+            np.save(f"{synth_dir}/std.npy", self.std)
+
+        include_features = []
+        # if exclude_features is not None:
+        #     for feature in feats:
+        #         if feature not in exclude_features:
+        #             include_features.append(feats.index(feature))
+
+        for i in range(X.shape[0]):
+            obs_val, obs_mask, mask, sample, obs_intact = parse_data(X[i], rate, is_test, length, include_features=include_features, forward_trial=forward_trial, random_trial=random_trial)
+            self.observed_values.append(obs_val)
+            self.observed_masks.append(obs_mask)
+            self.gt_masks.append(mask)
+            self.obs_data_intact.append(sample)
+            self.gt_intact.append(obs_intact)
+        self.gt_masks = torch.tensor(np.array(self.gt_masks), dtype=torch.float32)
+        self.observed_values = torch.tensor(np.array(self.observed_values), dtype=torch.float32)
+        self.obs_data_intact = np.array(self.obs_data_intact)
+        self.gt_intact = np.array(self.gt_intact)
+        self.observed_masks = torch.tensor(np.array(self.observed_masks), dtype=torch.float32)
+        # print(f"obs mask: {self.observed_values}\n\nmean: {}")
+        self.observed_values = ((self.observed_values - self.mean) / self.std) * self.observed_masks
+        self.obs_data_intact = ((self.obs_data_intact - self.mean) / self.std) * self.observed_masks.numpy()
+        self.gt_intact = ((self.gt_intact - self.mean) / self.std) * self.gt_masks.numpy()
+
+        
+    def __getitem__(self, index):
+        s = {
+            "observed_data": self.observed_values[index],
+            "observed_mask": self.observed_masks[index],
+            # "gt_mask": self.gt_masks[index],
+            "timepoints": np.arange(self.eval_length),
+            "gt_intact": self.gt_intact[index]
+        }
+        if len(self.gt_masks) == 0:
+            s["gt_mask"] = None
+        else:
+            s["gt_mask"] = self.gt_masks[index]
+        return s
+    
+    def __len__(self):
+        return len(self.observed_values)
+
+
+def get_dataloader(n_steps, n_features, num_seasons, batch_size=16, missing_ratio=0.1, seed=10, is_test=False, v2='v1',  noise=False, is_mcar=False, is_col_miss=None):
+    np.random.seed(seed=seed)
+    train_dataset = Synth_Dataset(n_steps, n_features, num_seasons, rate=missing_ratio, seed=seed, v2=v2, noise=noise, is_mcar=is_mcar, is_col_miss=is_col_miss)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    synth_dir = f"./data/synth/{v2}"
+    if os.path.exists(f"{synth_dir}/mean.npy"):
+        mean = np.load(f"{synth_dir}/mean.npy")
+    else:
+        mean = None
+    
+    if os.path.exists(f"{synth_dir}/std.npy"):
+        std = np.load(f"{synth_dir}/std.npy")
+    else:
+        std = None
+    test_dataset = Synth_Dataset(n_steps, n_features, 10, rate=missing_ratio, seed=seed*5, v2=v2, noise=noise, mean=mean, std=std)
+    if is_test:
+        test_loader = DataLoader(test_dataset, batch_size=batch_size)
+    else:
+        test_loader = DataLoader(test_dataset, batch_size=len(test_dataset))
+    return train_loader, test_loader, mean, std
+
+
+def get_testloader_synth(n_steps, n_features, num_seasons, batch_size=16, missing_ratio=0.2, seed=10, exclude_features=None, length=100, forecasting=False, random_trial=False, v2='v1', noise=False, mean=None, std=None):
+    np.random.seed(seed=seed)
+    if forecasting:
+        forward = n_steps - length
+        test_dataset = Synth_Dataset(n_steps, n_features, num_seasons, rate=missing_ratio, is_test=True, length=length, exclude_features=exclude_features, seed=seed, forward_trial=forward, v2=v2, noise=noise, mean=mean, std=std)
+    else:
+        test_dataset = Synth_Dataset(n_steps, n_features, num_seasons, rate=missing_ratio, is_test=True, length=length, exclude_features=exclude_features, seed=seed, random_trial=random_trial, v2=v2, noise=noise, mean=mean, std=std)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size)
+    return test_loader
---
+++ exe_synth_v3_mask.py
@@ -0,0 +1,186 @@
+from models.mask_main_model import Mask_Synth
+from datasets.dataset_synth_mask import get_dataloader
+from utils.utils import train, get_num_params, calc_quantile_CRPS, evaluate_imputation_all, clip_pattern_mask
+import numpy as np
+import torch
+import sys
+import os
+from pypots.imputation import SAITS
+import matplotlib.pyplot as plt
+import matplotlib
+from datasets.synthetic_data import feats_v3
+import pickle
+import json
+from json import JSONEncoder
+import math
+from tqdm import tqdm
+import xskillscore as xs
+import xarray as xr
+matplotlib.rc('xtick', labelsize=20) 
+matplotlib.rc('ytick', labelsize=20) 
+# torch.manual_seed(42)
+np.set_printoptions(threshold=np.inf)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+class NumpyArrayEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return JSONEncoder.default(self, obj)
+   
+
+seed = np.random.randint(10, 100)
+n_features = len(feats_v3)
+n_sample = 100
+d_time = 366
+config_dict_csdi = {
+    'train': {
+        'epochs': 200,
+        'batch_size': 4,
+        'lr': 1.0e-3
+    },      
+    'diffusion': {
+        'layers': 4, 
+        'channels': 64,
+        'nheads': 8,
+        'diffusion_embedding_dim': 128,
+        'beta_start': 0.0001,
+        'beta_end': 0.5,
+        'num_steps': 50,
+        'schedule': "quad",
+         'is_fast': False,
+    },
+    'model': {
+        'is_unconditional': True,
+        'timeemb': 128,
+        'featureemb': 16,
+        'target_strategy': "random",
+        'type': 'CSDI',
+        'n_layers': 3, 
+        'd_time': d_time,
+        'n_feature': n_features,
+        'd_model': 128,
+        'd_inner': 128,
+        'n_head': 8,
+        'd_k': 64,
+        'd_v': 64,
+        'dropout': 0.1,
+        'diagonal_attention_mask': True
+    },
+}
+
+nsample = 50
+
+n_steps = d_time
+
+filename = './data/v3/miss_data_v3.npy'
+train_loader, valid_loader, mean, std = get_dataloader(n_features=n_features, n_steps=n_steps, num_seasons=nsample, batch_size=32, missing_ratio=0.01, seed=seed, 
+                                            v2='v3', is_mcar=False, is_col_miss=None)
+
+model_csdi = Mask_Synth(config_dict_csdi, device, target_dim=n_features).to(device)
+model_folder = "./saved_model_mask_awn"
+filename = f"model_csdi_mask_v3.pth"
+if not os.path.isdir(model_folder):
+    os.makedirs(model_folder)
+print(f"\n\nCSDI Masked training starts.....\n")
+train(
+    model_csdi,
+    config_dict_csdi["train"],
+    train_loader,
+    valid_loader=None,
+    foldername=model_folder,
+    filename=f"{filename}",
+    is_saits=False
+)
+# model_csdi.load_state_dict(torch.load(f"{model_folder}/{filename}"))
+print(f"CSDI params: {get_num_params(model_csdi)}")
+
+
+def quantile_loss(target, forecast, q: float) -> float:
+    # print(f"in quant: target: {target.shape}, forecast: {forecast.shape}")
+    return 2 * torch.sum(
+        torch.abs((forecast - target) * ((target <= forecast) * 1.0 - q))
+    )
+
+def calc_denominator(target):
+    return torch.sum(torch.abs(target))
+
+def calc_quantile_CRPS(target, forecast, mean_scaler, scaler):
+    # print(f"target: {target.shape}\nforecast: {forecast.shape}")
+    target = target * scaler + mean_scaler
+    forecast = forecast * scaler + mean_scaler
+
+    # print(f"target: {target}")
+    # print(f"forecasts: {forecast[0:10]}")
+
+    quantiles = np.arange(0.05, 1.0, 0.05)
+    denom = calc_denominator(target)
+    CRPS = 0
+    for i in range(len(quantiles)):
+        q_pred = []
+        for j in range(len(forecast)):
+            q_pred.append(torch.quantile(forecast[j : j + 1], quantiles[i], dim=1))
+        q_pred = torch.cat(q_pred, 0)
+        q_loss = quantile_loss(target, q_pred, quantiles[i])
+        # print(f"q_loss: {q_loss}, denom: {denom}")
+        CRPS += q_loss / denom
+        # print(f"CRPS each qunatile: {CRPS}")
+    return CRPS.item() / len(quantiles)
+
+
+nsample = 20000 # 3000 * 4 * 8
+ground = 0
+for i, val in enumerate(valid_loader):
+    ground = val['observed_mask'].to(device).float() # (B, L, K)
+    # ground = ground.reshape(ground.shape[0], -1).cpu().numpy()
+
+sample_folder = './data/v3/miss_pattern'
+
+if not os.path.isdir(sample_folder):
+    os.makedirs(sample_folder)
+
+with torch.no_grad():
+    output = model_csdi.evaluate(nsample, (1, n_features, d_time))
+    samples = output
+
+    samples = samples.permute(0, 1, 3, 2)  # (B,nsample,L,K)
+    # samples = samples.reshape(samples.shape[0], samples.shape[1], -1).cpu().numpy()
+    # samples = (samples > 0).float()
+    # print(f"sample 1: {samples[0][1].cpu().numpy()}")
+    # print(f"sample 2: {samples[0][2].cpu().numpy()}")
+    # print(f"sample 3: {samples[0][3].cpu().numpy()}")
+    save_samples = clip_pattern_mask(samples.cpu().numpy())
+    save_samples = save_samples.squeeze(0)
+    # print(f"sample 1: {save_samples[1].cpu().numpy()}")
+    # print(f"sample 2: {save_samples[2].cpu().numpy()}")
+    # print(f"sample 3: {save_samples[3].cpu().numpy()}")
+    for i in range(save_samples.shape[0]):
+        np.save(f"{sample_folder}/pattern_{i}.npy", save_samples[i])
+
+    crps_avg = 0
+    num = 0
+    for i in range(len(ground)):
+        crps = calc_quantile_CRPS(ground[i].unsqueeze(0), samples, 0, 1)
+        print(f"CRPS for {i} : {crps}")
+        crps_avg += crps
+        num += 1
+    print(f"final CRPS: {crps_avg / num}")
+    
+
+    # forecasts = xr.DataArray(samples, coords=[('member', np.arange(samples.shape[0])), ('b', np.arange(1)), ('x', np.arange(n_features * d_time))])
+    # forecast_mean = np.mean(samples, axis=0)
+    # forcast_std = np.std(samples, axis=0)
+    # forecasts = st.norm.cdf(samples, ground.shape[0], ground.shape[1], )
+    # brier = xs.brier_score(observations, (forecasts).mean('member'), dim="b")
+    # crps = xs.crps_ensemble(observations, forecasts, member_dim='member', dim='b')
+
+    # print(f"brier: {brier}\ncrps: {crps}")
+    # print(f"CRPS: {crps}")
+
+
+
+
+    
+
+
