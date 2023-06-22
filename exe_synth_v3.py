324faf18c98e8c66bbbd751647c73260a8ea7a52
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-06-22 12:11:48 -0700

    synth v3 random model 1-9

--- exe_synth_v3.py
+++ exe_synth_v3.py
@@ -79,19 +79,19 @@ train_loader, valid_loader, mean, std = get_dataloader(n_steps, n_features, num_
 
 model_csdi = CSDI_Synth(config_dict_csdi, device, target_dim=len(given_features)).to(device)
 model_folder = "./saved_model_synth_v3"
-filename = f"model_csdi_synth_v3.pth"
+filename = f"model_csdi_synth_v3_{miss_type}.pth"
 if not os.path.isdir(model_folder):
     os.makedirs(model_folder)
 print(f"\n\nCSDI training starts.....\n")
-# train(
-#     model_csdi,
-#     config_dict_csdi["train"],
-#     train_loader,
-#     valid_loader=valid_loader,
-#     foldername=model_folder,
-#     filename=f"{filename}",
-#     is_saits=False
-# )
+train(
+    model_csdi,
+    config_dict_csdi["train"],
+    train_loader,
+    valid_loader=valid_loader,
+    foldername=model_folder,
+    filename=f"{filename}",
+    is_saits=False
+)
 model_csdi.load_state_dict(torch.load(f"{model_folder}/{filename}"))
 print(f"CSDI params: {get_num_params(model_csdi)}")
 
