6c60687004df94668005f9e31fb30ab01b70076f
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-06-23 09:16:54 -0700

    synth v3 random model 1-12

--- exe_synth_v3.py
+++ exe_synth_v3.py
@@ -83,15 +83,15 @@ filename = f"model_csdi_synth_v3_{miss_type}.pth"
 if not os.path.isdir(model_folder):
     os.makedirs(model_folder)
 print(f"\n\nCSDI training starts.....\n")
-train(
-    model_csdi,
-    config_dict_csdi["train"],
-    train_loader,
-    valid_loader=valid_loader,
-    foldername=model_folder,
-    filename=f"{filename}",
-    is_saits=False
-)
+# train(
+#     model_csdi,
+#     config_dict_csdi["train"],
+#     train_loader,
+#     valid_loader=valid_loader,
+#     foldername=model_folder,
+#     filename=f"{filename}",
+#     is_saits=False
+# )
 model_csdi.load_state_dict(torch.load(f"{model_folder}/{filename}"))
 print(f"CSDI params: {get_num_params(model_csdi)}")
 
@@ -169,18 +169,18 @@ print(f"\n\DiffSAITS training starts.....\n")
 
 # model_diff_saits.load_state_dict(torch.load(f"{model_folder}/{filename}"))
 
-# train(
-#     model_diff_saits,
-#     config_dict_diffsaits["train"],
-#     train_loader,
-#     valid_loader=valid_loader,
-#     foldername=model_folder,
-#     filename=f"{filename}",
-#     is_saits=True
-# )
+train(
+    model_diff_saits,
+    config_dict_diffsaits["train"],
+    train_loader,
+    valid_loader=valid_loader,
+    foldername=model_folder,
+    filename=f"{filename}",
+    is_saits=True
+)
 
-model_diff_saits.load_state_dict(torch.load(f"{model_folder}/{filename}"))
-# print(f"DiffSAITS params: {get_num_params(model_diff_saits)}")
+# model_diff_saits.load_state_dict(torch.load(f"{model_folder}/{filename}"))
+print(f"DiffSAITS params: {get_num_params(model_diff_saits)}")
 
 models = {
     'CSDI': model_csdi,
