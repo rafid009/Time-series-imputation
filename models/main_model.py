ba4ab2eca4ec2f4d684ac75afb8d7b4c1fcdc8a6
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-06-25 23:49:27 -0700

    fde 2nd v2, v3, v4, v5, v6, v7, 1-12

--- models/main_model.py
+++ models/main_model.py
@@ -33,7 +33,7 @@ class CSDI_base(nn.Module):
         input_dim = 1 if self.is_unconditional == True else 2
         if self.model_type == 'SAITS':
             self.is_saits = True
-            self.diffmodel = diff_SAITS_new_2(
+            self.diffmodel = diff_SAITS_new(
                 diff_steps=config['diffusion']['num_steps'],
                 n_layers=config['model']['n_layers'],
                 d_time=config['model']['d_time'],
