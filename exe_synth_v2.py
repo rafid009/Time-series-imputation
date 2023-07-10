c934c6e0260215439005539d49af056003a3d233
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2023-07-10 13:40:54 -0700

    synth experiments with 0.7 beta end 1-1

--- exe_synth_v2.py
+++ exe_synth_v2.py
@@ -118,7 +118,7 @@ config_dict_diffsaits = {
         'nheads': 8,
         'diffusion_embedding_dim': 128,
         'beta_start': 0.0001,
-        'beta_end': 0.5,
+        'beta_end': 0.7,
         'num_steps': 50,
         'schedule': "quad",
          'is_fast': False,
